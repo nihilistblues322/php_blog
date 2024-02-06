d0609edb0a9b9842521977a2375dcfa053709c0a
iuiuiuiuuiui <miserylovescompany228@gmail.com>
2024-02-06 15:32:24 +0600

    first

---
+++ app/controllers/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$title = 'My Blog :: Home';
+
+$posts = $db->query("SELECT * FROM posts ORDER BY id DESC")->fetchAll();
+$recent_posts = $db->query("SELECT * FROM posts ORDER BY id DESC LIMIT 3 ")->fetchAll();
+
+
+require_once VIEWS . '/index.tpl.php';
---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'host' => 'localhost',
+    'db_name' => 'test',
+    'username' => 'newroot',
+    'password' => 'some_password',
+    'charset' => 'utf8',
+    'options' => [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]
+];
