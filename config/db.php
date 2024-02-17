297d8be78e3a51b74fd81b1d0f7ae13658f9445f
iuiuiuiuuiui <miserylovescompany228@gmail.com>
2024-02-18 03:10:21 +0600

    chekpoint

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return [
     'host' => 'localhost',
     'dbname' => 'test',
-    'username' => 'root',
-    'password' => 'root',
+    'username' => 'newroot',
+    'password' => 'some_password',
     'charset' => 'utf8', // utf8mb4
     'options' => [
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
