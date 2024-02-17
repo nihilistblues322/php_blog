d6f265dbeee73635cddc3b24c0026f44c0cee2d1
iuiuiuiuuiui <miserylovescompany228@gmail.com>
2024-02-18 03:09:56 +0600

    chekpoint

--- public/test/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$file = __DIR__;
-require 'file1.php';
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '93d7b54658d9966d861134a3070e6c00496c8fc9',
+        'reference' => '910be9a5ef5a87c45bb3ec85011f1f4cec1af070',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '93d7b54658d9966d861134a3070e6c00496c8fc9',
+            'reference' => '910be9a5ef5a87c45bb3ec85011f1f4cec1af070',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
